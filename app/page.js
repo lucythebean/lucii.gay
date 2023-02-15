2a03c398b7803b381c8f3a2cbf8e7b7bd7c7b6e4
lucythebean <114861298+lucythebean@users.noreply.github.com>
2023-02-15 23:00:18 +1000

    Homepage css works for all mobile platforms

--- app/page.js
+++ app/page.js
@@ -5,22 +5,20 @@ import Link from 'next/link'
 
 import yupee from "../public/yupeeee.png"
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.header}><h1>Epic Swag homepage title</h1></div>
+      <div className={styles.header}><h1>Lucy's internet den :3</h1></div>
       <div className={styles.description}>
       <Image src={yupee} width={300} height={462} alt="A crudely drawn cat raising one eyebrow at viewer with a trans flag in the background"/>
       <p>A whole big chunk of text, a funky one if you will, lorem ipsum verde, a duck walked up to a lemonade stand and she said to the man, running the stand, hey! bababa, got any grapes? the man said no, we just sell lemonade, if you want grapes theres load at the store. the duck said idk i forgor and he waddled away wadddle waddle lorem ipsum verde avada kadavera ecetera overwatch winton bingled (:3) gayge testing 2 testing 3 testing 1(prequel) now i add more text to it because i need to test however down it goes and if its going to scale well with more word pog</p>
       </div>
       <div className={styles.grid}>
         <div>
-          <a href='/search'>
+          <Link href='/search'>
           <h2>Search</h2>
           <p>Parse yourself some Genshin api (Work in progress lol)</p>
-          </a>
+          </Link>
         </div>
         <div>
           <h2>More testing</h2>
