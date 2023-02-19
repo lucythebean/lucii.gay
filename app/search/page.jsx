98ceabe88819f545688a05e0fa3ec685a74e3100
lucythebean <114861298+lucythebean@users.noreply.github.com>
2023-02-19 15:39:13 +1000

    bugfix for parsing thing :3

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import React from "react";
 import styles from './page.module.css'
 import Link from 'next/link';
+import parse from './parse';
 
 
 export default function searchForm(){
