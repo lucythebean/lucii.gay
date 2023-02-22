eba5dc50f48932d9a7f361e0380fbafdd7fb8b1a
lucythebean <114861298+lucythebean@users.noreply.github.com>
2023-02-22 22:33:11 +1000

    Some class stuff is working!

--- public/parse.js
+++ public/parse.js
@@ -4,7 +4,7 @@ export function getPath(obj) {
   var keys = [];
   for (var key in obj) {
     keys.push(key);
-    if (typeof obj[key] === "object") {
+    if (typeof obj[key] !== "string") {
       var subkeys = getPath(obj[key]);
       keys = keys.concat(
         subkeys.map(function (subkey) {
@@ -13,12 +13,34 @@ export function getPath(obj) {
       );
     }
   }
+  console.log("keys: " + Object.keys(obj));
   return keys;
 }
 
-export function getKeys(obj){
-  var keys =[];
-  for (Object.keys(obj).length in obj){
-    
+export function getKeys(obj) {
+  var keys = [];
+  var keyList = Object.keys(obj);
+  console.log("keylist: " + keyList);
+  for (key in keyList) {
+    console.log("key: " + keyList[key]);
+    keys.push(Object.keys(obj[key]));
+    console.log("keys: " + Object.keys(obj[key]));
   }
+  console.log(keys);
+  return keys;
+}
+
+export function keyify(input) {
+  const keyify = (obj, prefix = "") =>
+    Object.keys(obj).reduce((res, el) => {
+      if (typeof obj[el] === "object" && obj[el] !== null) {
+        return [...res, ...keyify(obj[el], prefix + el + "|")];
+      }
+      return [...res, prefix + el];
+    }, []);
+  const output = keyify(input);
+  for (i in output) {
+    console.log(output[i].split("|")[0]);
+  }
+  return output;
 }
